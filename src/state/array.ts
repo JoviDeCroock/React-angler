c1d1e0facfa46b22cc5739047e617bd21794349d
Jovi De Croock <jovi@codifly.be>
2019-01-24 15:49:56 +0100

    chore: optimize for bundleSize

--- src/state/array.ts
+++ src/state/array.ts
@@ -3,6 +3,7 @@ import * as React from "react";
 interface UseArray {
   add: (element: any) => void;
   clear: () => void;
+  makeUnique: () => void;
   remove: (func: (value: Array<any>) => Array<any>) => void;
   removeByElement: (element: any) => void;
   removeByIndex: (index: number) => void;
@@ -10,14 +11,15 @@ interface UseArray {
   value: Array<any>;
 }
 
-export default (initialValue: Array<any> = []): UseArray => {
-  const { 0: value, 1: setValue  } = React.useState(initialValue);
+export default (initialValue?: Array<any>): UseArray => {
+  const { 0: value, 1: setValue  } = React.useState(initialValue || []);
   return {
-    add: React.useCallback((element) => setValue((val) => [...val, element]), [setValue]),
-    clear: React.useCallback(() => setValue(() => []), [setValue]),
-    remove: React.useCallback((func) => setValue((val) => func(val)), [setValue]),
-    removeByElement: React.useCallback((element) => setValue((val) => val.filter((e) => e !== element)), [setValue]),
-    removeByIndex: React.useCallback((index) => setValue((val) => val.filter((e, i) => i !== index)), [setValue]),
+    add: React.useCallback((element) => setValue((val) => [...val, element]), []),
+    clear: React.useCallback(() => setValue(() => []), []),
+    makeUnique: React.useCallback(() => setValue((val) => [...(new Set(val) as any)]), []),
+    remove: React.useCallback((func) => setValue((val) => func(val)), []),
+    removeByElement: React.useCallback((element) => setValue((val) => val.filter((e) => e !== element)), []),
+    removeByIndex: React.useCallback((index) => setValue((val) => val.filter((e, i) => i !== index)), []),
     setValue,
     value,
   };
