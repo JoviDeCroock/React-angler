f1ed1929f3acf34ebb408c2ae776b086ae4aa611
Max Syrykh <slogger1994@gmail.com>
2019-02-13 18:50:32 +0500

    chore: remove `= any` part

--- src/utils/getPreviousValue.ts
+++ src/utils/getPreviousValue.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-export default function usePreviousValue<T = any>(value: T): T {
+export default function usePreviousValue<T>(value: T): T {
   const ref = useRef(null);
   useEffect(() => { ref.current = value; });
   return ref.current;
