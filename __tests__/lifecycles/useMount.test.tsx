d952943080be950ad00348c074c5a706971997d0
Jovi De Croock <jovi@codifly.be>
2019-02-14 16:15:16 +0100

    tests: test didMount

---
+++ __tests__/lifecycles/useMount.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup, testHook } from 'react-testing-library';
+import useDidMount from '../../src/lifecycles/onMount';
+
+describe('useDidMount', () => {
+  afterEach(() => cleanup());
+
+  it('Should only mount once', () => {
+    const x = jest.fn();
+    const { rerender } = testHook(() => {
+      useDidMount(x)
+    });
+    expect(x).toBeCalledTimes(1);
+    rerender();
+    expect(x).toBeCalledTimes(1);
+  })
+});
