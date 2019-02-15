a98db2f2a822fcdcd435769be928056fd533b739
jdecroock <decroockjovi@gmail.com>
2019-02-15 17:04:55 +0100

    tests: didupdate

---
+++ __tests__/lifecycles/useDidUpdate.test.tsx
@@ -0,0 +1,20 @@
+import { cleanup, testHook } from 'react-testing-library';
+import useDidUpdate from '../../src/lifecycles/onUpdate';
+
+describe('useDidUpdate', () => {
+  afterEach(() => cleanup());
+
+  it('Should call the function when updating/mounting', () => {
+    const x = jest.fn();
+    let name = 'Jovi';
+    const { rerender } = testHook(() => {
+      useDidUpdate(x, [name])
+    });
+    expect(x).toBeCalledTimes(1);
+    rerender();
+    expect(x).toBeCalledTimes(1);
+    name = 'Liesse';
+    rerender();
+    expect(x).toBeCalledTimes(2);
+  })
+});
